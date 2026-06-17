1f37fa91b1bf9933071ea7ba88cbf99d49ac99c6
Christoph Mewes <christoph@kubermatic.com>
2026-06-17 17:36:49 +0200

    [account-operator] add apis
    Signed-off-by: Christoph Mewes <christoph@kubermatic.com>

---
+++ apis/core/v1alpha1/account_webhook.go
@@ -0,0 +1,106 @@
+package v1alpha1
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	mcruntime "sigs.k8s.io/multicluster-runtime"
+)
+
+func SetupAccountWebhookWithManager(mgr ctrl.Manager, organizationNameDenyList []string, accountTypeAllowList []AccountType) error {
+	return mcruntime.NewWebhookManagedBy(mgr).
+		For(&Account{}).
+		WithDefaulter(&AccountDefaulter{}).
+		WithValidator(&AccountValidator{OrganizationNameDenyList: organizationNameDenyList, AccountTypeAllowList: accountTypeAllowList}).
+		Complete()
+}
+
+type AccountDefaulter struct{}
+
+// Default implements admission.CustomDefaulter.
+func (a *AccountDefaulter) Default(ctx context.Context, obj runtime.Object) error {
+	account := obj.(*Account)
+
+	req, err := admission.RequestFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	account.Spec.Creator = &req.UserInfo.Username
+
+	return nil
+}
+
+var _ webhook.CustomDefaulter = &AccountDefaulter{} // nolint:staticcheck
+var _ webhook.CustomValidator = &AccountValidator{} // nolint:staticcheck
+
+type AccountValidator struct {
+	OrganizationNameDenyList []string
+	AccountTypeAllowList     []AccountType
+}
+
+func (v *AccountValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	account := obj.(*Account)
+	// accountType := string(account.Spec.Type)
+
+	if !slices.Contains(v.AccountTypeAllowList, account.Spec.Type) {
+		// metrics.WebhookValidations.WithLabelValues("create", "denied", accountType).Inc()
+		return nil, fmt.Errorf("account type %s not in allowlist", account.Spec.Type)
+	}
+
+	if account.Spec.Type != AccountTypeOrg {
+		// metrics.WebhookValidations.WithLabelValues("create", "allowed", accountType).Inc()
+		return nil, nil
+	}
+
+	if len(strings.TrimSpace(account.Name)) < 3 {
+		// metrics.WebhookValidations.WithLabelValues("create", "denied", accountType).Inc()
+		return nil, fmt.Errorf("organization name %q is too short, must be at least 3 characters", account.Name)
+	}
+
+	if slices.Contains(v.OrganizationNameDenyList, account.Name) {
+		// metrics.WebhookValidations.WithLabelValues("create", "denied", accountType).Inc()
+		return nil, fmt.Errorf("organization name %q is not allowed", account.Name)
+	}
+
+	// metrics.WebhookValidations.WithLabelValues("create", "allowed", accountType).Inc()
+	return nil, nil
+}
+
+func (v *AccountValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	account := newObj.(*Account)
+	// accountType := string(account.Spec.Type)
+
+	if !slices.Contains(v.AccountTypeAllowList, account.Spec.Type) {
+		// metrics.WebhookValidations.WithLabelValues("update", "denied", accountType).Inc()
+		return nil, fmt.Errorf("account type %s not in allowlist", account.Spec.Type)
+	}
+
+	if account.Spec.Type != AccountTypeOrg {
+		// metrics.WebhookValidations.WithLabelValues("update", "allowed", accountType).Inc()
+		return nil, nil
+	}
+
+	if len(strings.TrimSpace(account.Name)) < 3 {
+		// metrics.WebhookValidations.WithLabelValues("update", "denied", accountType).Inc()
+		return nil, fmt.Errorf("organization name %q is too short, must be at least 3 characters", account.Name)
+	}
+
+	if slices.Contains(v.OrganizationNameDenyList, account.Name) {
+		// metrics.WebhookValidations.WithLabelValues("update", "denied", accountType).Inc()
+		return nil, fmt.Errorf("organization name %q is not allowed", account.Name)
+	}
+
+	// metrics.WebhookValidations.WithLabelValues("update", "allowed", accountType).Inc()
+	return nil, nil
+}
+
+func (v *AccountValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	return nil, nil
+}
