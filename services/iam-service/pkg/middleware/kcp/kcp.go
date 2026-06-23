bd659426e12eb89332486a2a47c47b9a1c83443c
Christoph Mewes <christoph@kubermatic.com>
2026-06-23 17:42:40 +0200

    boilerplates
    On-behalf-of: @SAP christoph.mewes@sap.com
    
    Signed-off-by: Christoph Mewes <christoph@kubermatic.com>

--- services/iam-service/pkg/middleware/kcp/kcp.go
+++ services/iam-service/pkg/middleware/kcp/kcp.go
@@ -1,3 +1,19 @@
+/*
+Copyright The Platform Mesh Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package kcp
 
 import (
@@ -17,10 +33,10 @@ import (
 )
 
 type Middleware struct {
-	log                      *logger.Logger
-	tenantRetriever          idm.IDMTenantRetriever
-	excludedIDMTenants       []string
-	restcfg                  *rest.Config
+	log                *logger.Logger
+	tenantRetriever    idm.IDMTenantRetriever
+	excludedIDMTenants []string
+	restcfg            *rest.Config
 }
 
 func New(restcfg *rest.Config, excludedIDMTenants []string, tenantRetriever idm.IDMTenantRetriever, log *logger.Logger) *Middleware {
@@ -31,10 +47,10 @@ func New(restcfg *rest.Config, excludedIDMTenants []string, tenantRetriever idm.
 	restcfg.CertFile = ""
 
 	return &Middleware{
-		log:                      log,
-		tenantRetriever:          tenantRetriever,
-		excludedIDMTenants:       excludedIDMTenants,
-		restcfg:                  restcfg,
+		log:                log,
+		tenantRetriever:    tenantRetriever,
+		excludedIDMTenants: excludedIDMTenants,
+		restcfg:            restcfg,
 	}
 }
 
